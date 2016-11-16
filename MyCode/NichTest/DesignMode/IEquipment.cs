61f59ac41bbec58ba753dd972ee74b08fb657bd3
Nicholas <603104212@qq.com>
2016-11-16 15:23:24 +0800

    add comments for equipment interface

--- MyCode/NichTest/DesignMode/IEquipment.cs
+++ MyCode/NichTest/DesignMode/IEquipment.cs
@@ -7,14 +7,44 @@ namespace NichTest
 {
     public interface IEquipment
     {
+        /// <summary>
+        /// Initial epuipment
+        /// </summary>
+        /// <param name="inPara">input parameters for eupipment</param>
+        /// <param name="syn">synchronization or asynchronization</param>
+        /// <returns>is successful</returns>
         bool Initial(Dictionary<string, string> inPara, int syn = 0);
 
+        /// <summary>
+        /// config equipment
+        /// </summary>
+        /// <param name="syn">synchronization or asynchronization</param>
+        /// <returns></returns>
         bool Configure(int syn = 0);
 
+        /// <summary>
+        /// power on or shut down equipment
+        /// </summary>
+        /// <param name="isON">power on?</param>
+        /// <param name="syn">synchronization or asynchronization</param>
+        /// <returns>is successful</returns>
         bool OutPutSwitch(bool isON, int syn = 0);
 
+        /// <summary>
+        /// config the offset
+        /// </summary>
+        /// <param name="channel">the channel of equipment</param>
+        /// <param name="offset">offset value</param>
+        /// <param name="syn">synchronization or asynchronization</param>
+        /// <returns>is successful</returns>
         bool ConfigOffset(int channel, double offset, int syn = 0);
 
+        /// <summary>
+        /// change the output channel of equipment
+        /// </summary>
+        /// <param name="channel">the channel of equipment</param>
+        /// <param name="syn">synchronization or asynchronization</param>
+        /// <returns>is successful</returns>
         bool ChangeChannel(int channel, int syn = 0);
     }
 }
