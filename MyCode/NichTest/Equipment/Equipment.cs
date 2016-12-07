d0cbb4a6c2bfd67c30388052bbf9ea72155c2371
Nicholas <603104212@qq.com>
2016-12-07 09:29:48 +0800

    add more method

--- MyCode/NichTest/Equipment/Equipment.cs
+++ MyCode/NichTest/Equipment/Equipment.cs
@@ -7,22 +7,14 @@ namespace NichTest
 {
     public class Equipment: IEquipment
     {
-        protected IOPort myIO;
-
+        protected IOPort myIO = IOPort.GetIOPort();
         protected bool isConnected;
-
         protected bool isConfigured;
-
         protected string IOType;
-
         protected string address;
-
         protected string name;
-
         protected bool reset;
-
         protected int role;
-
         protected Dictionary<int, double> offsetByCh = new Dictionary<int, double>();
 
         public virtual bool Initial(Dictionary<string, string> inPara, int syn = 0)
@@ -41,6 +33,11 @@ namespace NichTest
         }
 
         public virtual bool ConfigOffset(int channel, double offset, int syn = 0)
+        {
+            return false;
+        }
+
+        public virtual bool ConfigOffset(int channel, double offset_VCC, double offset_ICC, int syn = 0)
         {
             return false;
         }
