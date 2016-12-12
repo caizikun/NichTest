839eb2417ff6f255c4a700cdd084293c7383c303
Nicholas <603104212@qq.com>
2016-12-12 11:39:28 +0800

    add lock for thread safety

--- MyCode/NichTest/Equipment/PowerSupply/E3631.cs
+++ MyCode/NichTest/Equipment/PowerSupply/E3631.cs
@@ -8,6 +8,8 @@ namespace NichTest
 {
     class E3631 : PowerSupply
     {
+        private static object syncRoot = new Object();
+
         public override bool Initial(Dictionary<string, string> inPara, int syn = 0)
         {
             try
@@ -30,11 +32,12 @@ namespace NichTest
                 switch (IOType)
                 {
                     case "GPIB":
-                        myIO = new IOPort(IOType, "GPIB0::" + address);
-                        myIO.IOConnect();
-                        myIO.WriteString("*IDN?");
-                        string content = myIO.ReadString();
-                        this.isConnected = content.Contains("E3631");
+                        lock (syncRoot)
+                        {
+                            myIO.WriteString(IOPort.Type.GPIB, "GPIB0::" + address, "*IDN?");
+                            string content = myIO.ReadString(IOPort.Type.GPIB, "GPIB0::" + address);
+                            this.isConnected = content.Contains("E3631");
+                        }
                         break;
 
                     default:
@@ -53,261 +56,288 @@ namespace NichTest
 
         public override bool Configure(int syn = 0)
         {
-            try
+            lock (syncRoot)
             {
-                if (this.isConfigured)//曾经设定过
+                try
                 {
+                    if (this.isConfigured)//曾经设定过
+                    {
+                        return true;
+                    }
+
+                    if (this.reset == true)
+                    {
+                        this.isConfigured = this.Reset();
+                    }
+
+                    if ((channel_DUT == 1) || (channel_DUT == 2))
+                    {
+                        ConfigVoltageCurrent(channel_DUT, voltage_DUT, current_DUT);
+                    }
+
+                    if ((channel_Source == 1) || (channel_Source == 2))
+                    {
+                        ConfigVoltageCurrent(channel_Source, voltage_Source, current_Source);
+                    }
+
+                    this.isConfigured = OutPutSwitch(true, syn) && this.isConfigured;
                     return true;
-                }
 
-                if (this.reset == true)
+                }
+                catch (Exception ex)
                 {
-                    this.isConfigured = this.Reset();
+                    Log.SaveLogToTxt(ex.Message);
+                    return false;
                 }
-
-                if ((channel_DUT == 1) || (channel_DUT == 2))
-                {
-                    ConfigVoltageCurrent(channel_DUT, voltage_DUT, current_DUT);
-                }
-
-                if ((channel_Source == 1) || (channel_Source == 2))
-                {
-                    ConfigVoltageCurrent(channel_Source, voltage_Source, current_Source);
-                }
-
-                this.isConfigured = OutPutSwitch(true, syn) && this.isConfigured;
-                return true;
-
-            }
-            catch (Exception ex)
-            {
-                Log.SaveLogToTxt(ex.Message);
-                return false;
             }
         }
 
         public bool Reset()
         {
-            if (myIO.WriteString("*RST"))
+            lock (syncRoot)
             {
-                Thread.Sleep(3000);
-                return true;
-            }
-            else
-            {
-                return false;
+                if (myIO.WriteString(IOPort.Type.GPIB, "GPIB0::" + address, "*RST"))
+                {
+                    Thread.Sleep(3000);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
             }
         }
 
         public override bool OutPutSwitch(bool isON, int syn = 0)
         {
-            // "ON "" OFF"            
-            string command;
-            int delay = 0;
-            string intswitch;
+            lock (syncRoot)
+            {
+                // "ON "" OFF"            
+                string command;
+                int delay = 0;
+                string intswitch;
 
-            if (isON)
-            {
-                command = "ON";
-                intswitch = "1";
-                delay = this.openDelay;
-            }
-            else
-            {
-                command = "OFF";
-                intswitch = "0";
-                delay = this.closeDelay;
-            }
-
-            try
-            {
-                bool result = false;
-                if (syn == 0)
+                if (isON)
                 {
-                    result = myIO.WriteString("OUTP " + command);
-                    Thread.Sleep(delay);
-                    return result;
+                    command = "ON";
+                    intswitch = "1";
+                    delay = this.openDelay;
                 }
                 else
                 {
-                    for (int i = 0; i < 3; i++)
-                    {
-                        result = myIO.WriteString("OUTP " + command);
-                        if (result == true)
-                        {
-                            break;
-                        }
-                    }
+                    command = "OFF";
+                    intswitch = "0";
+                    delay = this.closeDelay;
+                }
 
-                    if (result == true)
+                try
+                {
+                    bool result = false;
+                    if (syn == 0)
                     {
-                        int k;
-                        for (k = 0; k < 3; k++)
+                        result = myIO.WriteString(IOPort.Type.GPIB, "GPIB0::" + address, "OUTP " + command);
+                        Thread.Sleep(delay);
+                        return result;
+                    }
+                    else
+                    {
+                        for (int i = 0; i < 3; i++)
                         {
-                            myIO.WriteString("OUTP?");
-                            string readtemp = myIO.ReadString();
-                            if (readtemp == intswitch + "\n")
+                            result = myIO.WriteString(IOPort.Type.GPIB, "GPIB0::" + address, "OUTP " + command);
+                            if (result == true)
                             {
                                 break;
                             }
                         }
 
-                        if (k <= 3)
+                        if (result == true)
                         {
-                            result = true;
-                        }
-                        else
-                        {
-                            result = false;
-                        }
+                            int k;
+                            for (k = 0; k < 3; k++)
+                            {
+                                myIO.WriteString(IOPort.Type.GPIB, "GPIB0::" + address, "OUTP?");
+                                string readtemp = myIO.ReadString(IOPort.Type.GPIB, "GPIB0::" + address);
+                                if (readtemp == intswitch + "\n")
+                                {
+                                    break;
+                                }
+                            }
 
-                        Thread.Sleep(delay);
+                            if (k <= 3)
+                            {
+                                result = true;
+                            }
+                            else
+                            {
+                                result = false;
+                            }
+
+                            Thread.Sleep(delay);
+                        }
+                        return result;
                     }
-                    return result;
                 }
-            }
-            catch (Exception ex)
-            {
-                Log.SaveLogToTxt(ex.ToString());
-                return false;
+                catch (Exception ex)
+                {
+                    Log.SaveLogToTxt(ex.ToString());
+                    return false;
+                }
             }
         }
 
-        public override bool ConfigOffset(int channel, double offset, int syn = 0)
+        public override bool ConfigOffset(int channel, double offset_VCC, double offset_ICC, int syn = 0)
         {
-            this.voltageOffset = offset;
-            return true;
+            lock (syncRoot)
+            {
+                this.voltageOffset = offset_VCC;
+                this.currentOffset = offset_ICC;
+                return true;
+            }
         }
 
         protected bool ConfigVoltageCurrent(int channel, double voltage, double current)
         {
-            string command = "";//= "APPL P6V," + str_V + "," + Str_I;
-            string command_channel = "";
-            voltage += voltageOffset;
+            lock (syncRoot)
+            {
+                string command = "";//= "APPL P6V," + str_V + "," + Str_I;
+                string command_channel = "";
+                voltage += voltageOffset;
 
-            if (channel == 1)
-            {
-                command = "APPL P6V," + voltage + "," + current;
-                command_channel = "APPL P6V";
-            }
-            else if (channel == 2)
-            {
-                command = "APPL P25V," + voltage + "," + current;
-                command_channel = "APPL P25V";
-            }
+                if (channel == 1)
+                {
+                    command = "APPL P6V," + voltage + "," + current;
+                    command_channel = "APPL P6V";
+                }
+                else if (channel == 2)
+                {
+                    command = "APPL P25V," + voltage + "," + current;
+                    command_channel = "APPL P25V";
+                }
 
-            try
-            {
-                Log.SaveLogToTxt("E3631 channel is " + command_channel + " voltage is " + voltage.ToString("f3") + " current is" + current);
-                return myIO.WriteString(command);                
-            }
-            catch (Exception ex)
-            {
-                Log.SaveLogToTxt(ex.ToString());
-                return false;
+                try
+                {
+                    Log.SaveLogToTxt("E3631 channel is " + command_channel + " voltage is " + voltage.ToString("f3") + " current is" + current);
+                    return myIO.WriteString(IOPort.Type.GPIB, "GPIB0::" + address, command);
+                }
+                catch (Exception ex)
+                {
+                    Log.SaveLogToTxt(ex.ToString());
+                    return false;
+                }
             }
         }
 
         public override bool ConfigVoltageCurrent(double voltage, int syn = 0)
         {
-            double volandoffset = voltage + voltageOffset;
-            bool flag = false;
-            string command = "";//= "APPL P6V," + str_V + "," + Str_I;
-            string command_channel = "";
-            if (Convert.ToInt16(channel_DUT) == 1)
+            lock (syncRoot)
             {
-                command = "APPL P6V," + volandoffset + "," + current_DUT;
-                command_channel = "APPL P6V";
-            }
-            else if (Convert.ToInt16(channel_DUT) == 2)
-            {
-                command = "APPL P25V," + volandoffset + "," + current_DUT;
-                command_channel = "APPL P25V";
-            }
-            syn = 0;
-            try
-            {
-                if (syn == 0)
+                double volandoffset = voltage + voltageOffset;
+                bool flag = false;
+                string command = "";//= "APPL P6V," + str_V + "," + Str_I;
+                string command_channel = "";
+                if (Convert.ToInt16(channel_DUT) == 1)
                 {
-                    myIO.WriteString(command);
-                    myIO.WriteString("*opc?");
-                    string StrTemp = myIO.ReadString().Replace("\n", "");
-                    if (StrTemp == "1")
-                    {
-                        flag = true;
-                    }
-                    else
-                    {
-                        flag = false;
-                    }
-
-                    // string Str = GetVoltage().ToString();
-                    Log.SaveLogToTxt("E3631 channel is " + command_channel + " voltage is " + volandoffset.ToString("f3") + " current is " + current_DUT);
-                    return flag;
+                    command = "APPL P6V," + volandoffset + "," + current_DUT;
+                    command_channel = "APPL P6V";
                 }
-                else// 因为读取当前的电压值 会导致仪器脱离程控, 不适用同步模式
+                else if (Convert.ToInt16(channel_DUT) == 2)
                 {
-                    flag = false;
-
-                    for (int i = 0; i < 3; i++)
+                    command = "APPL P25V," + volandoffset + "," + current_DUT;
+                    command_channel = "APPL P25V";
+                }
+                syn = 0;
+                try
+                {
+                    if (syn == 0)
                     {
-
-                        flag = myIO.WriteString(command);
-                        Thread.Sleep(500);
-                        // MyIO.WriteString(str_channel + "?");
-                        string StrVoltage = GetVoltage().ToString();
-                        // double SetVoltage = vol + offset;
-                        double MeasurtVoltage = Convert.ToDouble(StrVoltage);
-                        if (MeasurtVoltage <= volandoffset * 1.005 && MeasurtVoltage >= volandoffset * 0.995)
+                        myIO.WriteString(IOPort.Type.GPIB, "GPIB0::" + address, command);
+                        myIO.WriteString(IOPort.Type.GPIB, "GPIB0::" + address, "*opc?");
+                        string StrTemp = myIO.ReadString(IOPort.Type.GPIB, "GPIB0::" + address).Replace("\n", "");
+                        if (StrTemp == "1")
                         {
                             flag = true;
-                            Log.SaveLogToTxt("E3631 channel is " + command_channel + " voltage is " + volandoffset.ToString("f3") + " current is " + current_DUT);
-                            break;
                         }
+                        else
+                        {
+                            flag = false;
+                        }
+
+                        // string Str = GetVoltage().ToString();
+                        Log.SaveLogToTxt("E3631 channel is " + command_channel + " voltage is " + volandoffset.ToString("f3") + " current is " + current_DUT);
+                        return flag;
                     }
-                    if (!flag)
+                    else// 因为读取当前的电压值 会导致仪器脱离程控, 不适用同步模式
                     {
-                        Log.SaveLogToTxt("E3631 channel is " + command_channel + " voltage is " + volandoffset.ToString("f3") + " current is " + current_DUT + "error");
+                        flag = false;
+
+                        for (int i = 0; i < 3; i++)
+                        {
+
+                            flag = myIO.WriteString(IOPort.Type.GPIB, "GPIB0::" + address, command);
+                            Thread.Sleep(500);
+                            // MyIO.WriteString(str_channel + "?");
+                            string StrVoltage = GetVoltage().ToString();
+                            // double SetVoltage = vol + offset;
+                            double MeasurtVoltage = Convert.ToDouble(StrVoltage);
+                            if (MeasurtVoltage <= volandoffset * 1.005 && MeasurtVoltage >= volandoffset * 0.995)
+                            {
+                                flag = true;
+                                Log.SaveLogToTxt("E3631 channel is " + command_channel + " voltage is " + volandoffset.ToString("f3") + " current is " + current_DUT);
+                                break;
+                            }
+                        }
+                        if (!flag)
+                        {
+                            Log.SaveLogToTxt("E3631 channel is " + command_channel + " voltage is " + volandoffset.ToString("f3") + " current is " + current_DUT + "error");
+                        }
+                        return flag;
                     }
-                    return flag;
                 }
-            }
-            catch
-            {
-                Log.SaveLogToTxt("E3631 channel is " + command_channel + " voltage is " + volandoffset.ToString("f3") + " current is " + current_DUT + "error");
-                return false;
+                catch
+                {
+                    Log.SaveLogToTxt("E3631 channel is " + command_channel + " voltage is " + volandoffset.ToString("f3") + " current is " + current_DUT + "error");
+                    return false;
+                }
             }
         }
 
         public override double GetCurrent()
         {
-            double current = 0;
-            try
+            lock (syncRoot)
             {
-                myIO.WriteString("MEAS:CURR? P6V");
-                current = (Convert.ToDouble((myIO.ReadString(25)))) * 1000;
-                return current;
-            }
-            catch (Exception ex)
-            {
-                Log.SaveLogToTxt(ex.ToString());
-                return current;
+                try
+                {
+                    myIO.WriteString(IOPort.Type.GPIB, "GPIB0::" + address, "MEAS:CURR? P6V");
+                    double current = (Convert.ToDouble((myIO.ReadString(IOPort.Type.GPIB, "GPIB0::" + address, 25)))) * 1000;
+                    // 因为读取当前的电压值(电流)，会导致仪器脱离程控(一直在执行指令), 所以加延时等仪器恢复。                    
+                    Thread.Sleep(10000);
+                    return current - currentOffset;
+                }
+                catch (Exception ex)
+                {
+                    Log.SaveLogToTxt(ex.ToString());
+                    return Algorithm.MyNaN;
+                }
             }
         }
 
         public override double GetVoltage()
         {
-            double voltage = 0;
-            try
+            lock (syncRoot)
             {
-                myIO.WriteString("MEAS:VOLT? P6V");
-                voltage = Convert.ToDouble((myIO.ReadString(10)));
-                return voltage;
-            }
-            catch (Exception ex)
-            {
-                Log.SaveLogToTxt(ex.ToString());
-                return voltage;
+                try
+                {  
+                    myIO.WriteString(IOPort.Type.GPIB, "GPIB0::" + address, "MEAS:VOLT? P6V");
+                    double voltage = Convert.ToDouble((myIO.ReadString(IOPort.Type.GPIB, "GPIB0::" + address, 10)));
+                    // 因为读取当前的电压值(电流)，会导致仪器脱离程控(一直在执行指令), 所以加延时等仪器恢复。                    
+                    Thread.Sleep(10000);
+                    return voltage;
+                }
+                catch (Exception ex)
+                {
+                    Log.SaveLogToTxt(ex.ToString());
+                    return Algorithm.MyNaN;
+                }
             }
         }
     }
--- MyCode/NichTest/Equipment/PowerSupply/PowerSupply.cs
+++ MyCode/NichTest/Equipment/PowerSupply/PowerSupply.cs
@@ -8,33 +8,24 @@ namespace NichTest
     public class PowerSupply : Equipment
     {
         protected int closeDelay;
-
         protected int openDelay;
-
         protected double currentOffset;
-
         protected double voltageOffset;
-
         protected int channel_DUT;
-
         protected double current_DUT;
-
         protected double voltage_DUT;
-
         protected int channel_Source;
-
         protected double current_Source;
-
         protected double voltage_Source;
         
         public virtual double GetCurrent()
         {
-            return -1.0;
+            return Algorithm.MyNaN;
         }
 
         public virtual double GetVoltage()
         {
-            return -1.0;
+            return Algorithm.MyNaN;
         }
 
         public virtual bool ConfigVoltageCurrent(double voltage, int syn = 0) { return true; }
