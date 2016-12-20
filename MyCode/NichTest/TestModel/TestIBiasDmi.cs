cc676d3b93d85639f18f069d0cbf7a459a8e6e3f
Nicholas <603104212@qq.com>
2016-12-20 14:59:45 +0800

    add log to show detailed status

--- MyCode/NichTest/TestModel/TestIBiasDmi.cs
+++ MyCode/NichTest/TestModel/TestIBiasDmi.cs
@@ -9,24 +9,29 @@ namespace NichTest
     {
         public bool BeginTest(DUT dut, Dictionary<string, IEquipment> equipments, Dictionary<string, string> inPara)
         {
+            //get the current test channel
+            int channel = ConditionParaByTestPlan.Channel;
             try
             {
-                //get the current test channel
-                int channel = ConditionParaByTestPlan.Channel;
-                Log.SaveLogToTxt("Start to do IBiasDMI test for channel " + channel);
-                // open apc
-                Log.SaveLogToTxt("Close apc for module.");
-                dut.CloseAndOpenAPC(Convert.ToByte(DUT.APCMODE.IBAISandIMODON));
-                // read IBiasDMI
-                Log.SaveLogToTxt("Try to readDmiBias of DUT");
-                double ibiasDmi = dut.ReadDmiBias(channel);
-                Log.SaveLogToTxt("Read IBiasDMI is " + ibiasDmi.ToString("f3"));
-                Log.SaveLogToTxt("End DMI_IBias test for channel " + channel + "\r\n");
-                return true;
+                //have to lock this object, due to it can't run parallel test with TestTxPowerDmi
+                //as TestTxPowerDmi will disable TxPower, ibiasDmi will be defected.
+                lock (dut)
+                {
+                    Log.SaveLogToTxt("Start to do IBiasDMI test for channel " + channel);
+                    // open apc
+                    Log.SaveLogToTxt("Close apc for module.");
+                    dut.CloseAndOpenAPC(Convert.ToByte(DUT.APCMODE.IBAISandIMODON));
+                    // read IBiasDMI
+                    Log.SaveLogToTxt("Try to readDmiBias of DUT");
+                    double ibiasDmi = dut.ReadDmiBias(channel);
+                    Log.SaveLogToTxt("Read IBiasDMI is " + ibiasDmi.ToString("f3"));
+                    Log.SaveLogToTxt("End DMI_IBias test for channel " + channel + "\r\n");
+                    return true;
+                }
             }
             catch
             {
-                Log.SaveLogToTxt("Failed DMI_IBias test.");
+                Log.SaveLogToTxt("Failed DMI_IBias test for channel " + channel + "\r\n");
                 return false;
             }
         }
