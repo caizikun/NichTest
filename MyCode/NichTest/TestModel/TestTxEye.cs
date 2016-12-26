f83a24f5fb19d755591805079f5e26c4eb3ba1eb
Nicholas <603104212@qq.com>
2016-12-26 09:55:13 +0800

    add some new test model

---
+++ MyCode/NichTest/TestModel/TestTxEye.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NichTest
+{
+    class TestTxEye : ITest
+    {
+        public bool BeginTest(DUT dut, Dictionary<string, IEquipment> equipments, Dictionary<string, string> inPara)
+        {
+            //get the current test channel
+            int channel = ConditionParaByTestPlan.Channel;
+            try
+            {                
+                //get equipment object
+                Scope scope = (Scope)equipments["SCOPE"];
+
+                ////change to Tx path, no need this switch, otherwise it can't run parallel test
+                //if (equipments.Keys.Contains("NA_OPTICALSWITCH"))
+                //{
+                //    OpticalSwitch opticalSwitch = (OpticalSwitch)equipments["NA_OPTICALSWITCH"];
+                //    opticalSwitch.CheckEquipmentRole(1, (byte)channel);
+                //}
+
+                lock (scope)
+                {                    
+                    Log.SaveLogToTxt("Start to do TxEye test for channel " + channel);
+
+                    //prepare environment
+                    if (scope.SetMaskAlignMethod(1) && scope.SetMode(0) && scope.MaskONOFF(false) &&
+                      scope.SetRunTilOff() && scope.RunStop(true) && scope.OpenOpticalChannel(true) &&
+                      scope.RunStop(true) && scope.ClearDisplay() && scope.AutoScale())
+                    {
+                        Log.SaveLogToTxt("PrepareEnvironment OK!");
+                    }
+                    else
+                    {
+                        Log.SaveLogToTxt("PrepareEnvironment Fail!");
+                        return false;
+                    }
+
+                    // open apc
+                    Log.SaveLogToTxt("Close apc for module.");
+                    dut.CloseAndOpenAPC(Convert.ToByte(DUT.APCMODE.IBAISandIMODON));
+
+                    //Algorithm.GetSpec(specParameters, "MASKMARGIN(%)", 0, out MaskSpecMax, out MaskSpecMin);
+                    double MaskSpecMax = 255, MaskSpecMin = -255;
+
+                    //TxEye test
+                    Dictionary<string, double> outData = new Dictionary<string, double>();
+                    bool flagMask = false;
+                    scope.OpticalEyeTest(ref outData, 1);
+
+                    if (outData["MASKMARGIN(%)"] > MaskSpecMax || outData["MASKMARGIN(%)"] < MaskSpecMin)
+                    {
+                        flagMask = false;
+                    }
+                    else
+                    {
+                        flagMask = true;
+                    }
+
+                    //retest
+                    if (!flagMask)//后三次测试有一次失败就不在测试
+                    {
+                        for (int k = 0; k < 3; k++)
+                        {
+                            scope.OpticalEyeTest(ref outData);
+
+                            if (outData["MASKMARGIN(%)"] > MaskSpecMax || outData["MASKMARGIN(%)"] < MaskSpecMin)
+                            {
+                                break;
+                            }
+                        }
+                    }
+
+                    foreach (string key in outData.Keys)
+                    {
+                        Log.SaveLogToTxt(key + " = " + outData[key].ToString("f2"));
+                    }
+                    Log.SaveLogToTxt("End tx eye test for channel " + channel + "\r\n");
+                    return true;
+                }
+            }
+            catch
+            {
+                Log.SaveLogToTxt("Failed tx eye test for channel " + channel + "\r\n");
+                return false;
+            }
+        }
+
+        public bool SaveTestData()
+        {
+            return true;
+        }
+    }
+}
---
+++ MyCode/NichTest/TestModel/TestTxPowerDmi.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace NichTest
+{
+    public class TestTxPowerDmi : ITest
+    {
+        public bool BeginTest(DUT dut, Dictionary<string, IEquipment> equipments, Dictionary<string, string> inPara)
+        {
+            //get the current test channel
+            int channel = ConditionParaByTestPlan.Channel;
+            try
+            { 
+                //get equipment object
+                Scope scope = (Scope)equipments["SCOPE"];
+
+                lock (scope)
+                {                    
+                    Log.SaveLogToTxt("Start to do TxPowerDmi test for channel " + channel);
+
+                    //prepare environment
+                    if (scope.SetMaskAlignMethod(1) &&
+                    scope.SetMode(0) &&
+                    scope.MaskONOFF(false) &&
+                    scope.SetRunTilOff() &&
+                    scope.RunStop(true) &&
+                    scope.OpenOpticalChannel(true) &&
+                    scope.RunStop(true) &&
+                    scope.ClearDisplay() &&
+                    scope.AutoScale(1)
+                    )
+                    {
+                        Log.SaveLogToTxt("PrepareEnvironment OK!");
+                    }
+                    else
+                    {
+                        Log.SaveLogToTxt("PrepareEnvironment Fail!");
+                        return false;
+                    }
+
+                    //test dmi
+                    Log.SaveLogToTxt("Read DCA TxPower");
+                    double txDCAPowerDmi = scope.GetAveragePowerdBm();
+                    Log.SaveLogToTxt("txDCAPowerDmi = " + txDCAPowerDmi.ToString("f2"));
+                    Log.SaveLogToTxt("Read DUT Txpower");
+                    double txPowerDmi = dut.ReadDmiTxP(channel);
+                    Log.SaveLogToTxt("txPowerDmi = " + txPowerDmi.ToString("f2"));
+                    double txDmiPowerErr = txPowerDmi - txDCAPowerDmi;
+                    Log.SaveLogToTxt("txDmiPowerErr = " + txDmiPowerErr.ToString("f2"));
+
+                    //test disable power
+                    //have to lock this object, due to it can't run parallel test with TestIBiasDmi
+                    //as this will disable TxPower, ibiasDmi will be defected.
+                    lock (dut)
+                    {
+                        dut.SetSoftTxDis();
+                        scope.ClearDisplay();
+                        double txDisablePower = scope.GetAveragePowerdBm();
+                        Log.SaveLogToTxt("txDisablePower = " + txDisablePower.ToString());
+                        Thread.Sleep(200);
+                        if (!dut.TxAllChannelEnable())
+                        {
+                            scope.ClearDisplay();
+                            Thread.Sleep(200);
+                            return false;
+                        }
+                    }
+                    scope.ClearDisplay();
+                    Thread.Sleep(200);
+                    Log.SaveLogToTxt("End TxPowerDmi test " + channel + "\r\n");
+                    return true;
+                }
+            }
+            catch
+            {
+                Log.SaveLogToTxt("Failed TxPowerDmi test for channel " + channel + "\r\n");
+                return false;
+            }
+        }
+
+        public bool SaveTestData()
+        {
+            return true;
+        }
+    }
+}
